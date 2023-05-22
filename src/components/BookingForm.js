1e7d4ce9274857dc1a0368180bff3308895e7ce5
osagie amadasun <osagieamadasun123@gmail.com>
2023-05-22 12:41:16 +0100

    idk what i'm doing man

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,34 +2,43 @@ import React, { useState } from "react";
 
 function BookingForm() {
   // eslint-disable-next-line
-  const [availableTimes, setAvailableTimes] = useState({
-    date: "",
-    time: "",
-    guests: 1,
-    occasion: "Birthday",
-  });
+  //state for the form data
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [numberOfGuests, setNumberOfGuests] = useState();
+  const[occasions, setOccasions] = useState()
+
+
+  const fetchData = () => {
+    fetch('')
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setAvailableTimes((prevAvailableTimes) => ({
-      ...prevAvailableTimes,
-      [name]: value,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [time]: value,
+      [date]: value
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log(availableTimes);
   };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("it has been submitted👍");
+  }
 
   return (
     <div>
-      <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
+      <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }} onSubmit={handleSubmit}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input type="date" id="res-date" value={date} onChange={handleChange}/>
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time">
+        <select id="res-time" value={time} onChange={handleChange}>
           <option>17:00</option>
           <option>18:00</option>
           <option>19:00</option>
@@ -38,15 +47,14 @@ function BookingForm() {
           <option>22:00</option>
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input type="number" placeholder="1" min="1" max="10" id="guests" value={numberOfGuests} onChange={handleChange}/>
         <label htmlFor="occasion">Occasion</label>
         <select id="occasion">
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
-        <input type="submit" value="Make Your reservation" />
+        <button type="submit" onClick={handleClick}>Book reservations</button>
       </form>
-      );
     </div>
   );
 }
