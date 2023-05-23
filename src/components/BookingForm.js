6c9ccf92f5feb9055111df1f978cf2f200bf298c
osagie amadasun <osagieamadasun123@gmail.com>
2023-05-23 05:22:05 +0100

    added something

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
 
-function BookingForm() {
-  // eslint-disable-next-line
-  //state for the form data
-  const [date, setDate] = useState();
-  const [time, setTime] = useState();
-  const [numberOfGuests, setNumberOfGuests] = useState();
-  const[occasions, setOccasions] = useState()
+function BookingForm({ availableTimes, dispatch }) {
+  const [formData, setFormData] = useState({
+    date: '',
+    bookingTime: {availableTimes},
+    numberOfGuests: '',
+    occasion: ''
+  });
 
-
-  const fetchData = () => {
-    fetch('')
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [time]: value,
-      [date]: value
-    }));
+  const handleDateChange = (e) => {
+    const selectedDate = e.target.value;
+    dispatch({ type: 'UPDATE_TIMES', payload: selectedDate });
   };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
+    console.log(formData)
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(
+      (prev) => {
+        return {...prev, [name]: value};
+      })
+  };
+    // const fetchData = () => {
+    //   fetch('https://raw.githubusercontent.com/Meta-Front-End-Developer-PC/capstone/master/api.js')
+    //   .then(response => response.json())
+    //   .then(jsonData => setFormData(jsonData))
+    // }
+
+    const handleClick = (e) => {
+        e.preventDefault();
     console.log("it has been submitted👍");
   }
 
@@ -36,9 +41,9 @@ function BookingForm() {
     <div>
       <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }} onSubmit={handleSubmit}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" value={date} onChange={handleChange}/>
+        <input type="date" id="res-date" value={formData.date} onChange={handleDateChange} />
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time" value={time} onChange={handleChange}>
+        <select id="res-time" value={formData.bookingTime} onChange={handleChange}>
           <option>17:00</option>
           <option>18:00</option>
           <option>19:00</option>
@@ -47,13 +52,13 @@ function BookingForm() {
           <option>22:00</option>
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" value={numberOfGuests} onChange={handleChange}/>
+        <input type="number" placeholder="1" min="1" max="10" id="guests" value={formData.numberOfGuests} onChange={handleChange} />
         <label htmlFor="occasion">Occasion</label>
         <select id="occasion">
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
-        <button type="submit" onClick={handleClick}>Book reservations</button>
+        <button type="submit">Book reservations</button>
       </form>
     </div>
   );
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import BookingForm from './BookingForm'
 
 function BookingPage() {
-  const [availableTimes, setAvailableTimes] = useState();
-  const [resDate, setResDate] = useState('');
-  const [guests, setGuests] = useState(1);
-  const [occasion, setOccasion] = useState('');
+  const [availableTimes, dispatch] = useReducer(reducer, ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
 
 
-  
-  const handleResDateChange = (e) => {
-    setResDate(e.target.value);
-  };
+  //idek what is going on in this block¯\_(ツ)_/¯
+function reducer(availableTimes, action) {
+  switch (action.type) {
+    case 'UPDATE_TIMES':
+      const selectedTime = action.payload;
 
-  const handleResTimeChange = (e) => {
-    setAvailableTimes(e.target.value);
-  };
+      switch (selectedTime) {
+        case '17:00':
+          return ['17:00', '18:00', '19:00'];
+        case '18:00':
+          return ['18:00', '19:00', '20:00'];
+        case '19:00':
+          return ['19:00', '20:00', '21:00'];
+        case '20:00':
+          return ['20:00', '21:00', '22:00'];
+        case '21:00':
+          return ['21:00', '22:00'];
+        case '22:00':
+          return ['22:00'];
+        default:
+          return availableTimes;
+      }
 
-  const handleGuestsChange = (e) => {
-    setGuests(parseInt(e.target.value));
-  };
-
-  const handleOccasionChange = (e) => {
-    setOccasion(e.target.value);
-  };
+    default:
+      return availableTimes;
+  }
+}
+  function initializeTimes() {
+    return ['10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM'];
+  }
 
 
 
